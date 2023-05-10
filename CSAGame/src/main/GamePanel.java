a2f3f2e2f5e1d37fd9f406d117124f28364e8d73
TheChoco1 <thechocouno@gmail.com>
2023-05-10 09:10:34 -0400

    sprite + animation
    temp sprite + animation and cleaned up some code

--- CSAGame/src/main/GamePanel.java
+++ CSAGame/src/main/GamePanel.java
@@ -7,12 +7,14 @@ import java.awt.Graphics2D;
 
 import javax.swing.JPanel;
 
+import entity.Player;
+
 public class GamePanel extends JPanel implements Runnable {
 	
 	final int originalTitleSize = 16;
 	final int scale = 3;
 	
-	final int tileSize = originalTitleSize * scale;
+	public final int tileSize = originalTitleSize * scale;
 	final int maxScreenCol = 16;
 	final int maxScreenRow = 12;
 	final int screenWidth = tileSize * maxScreenCol;
@@ -22,10 +24,7 @@ public class GamePanel extends JPanel implements Runnable {
 	
 	KeyHandler keyH = new KeyHandler();
 	Thread gameThread;
-	
-	int playerX = 100;
-	int playerY = 100;
-	int playerSpeed = 4;
+	Player player = new Player(this, keyH);
 	
 	public GamePanel() {
 		this.setPreferredSize(new Dimension(screenWidth, screenHeight));
@@ -72,28 +71,14 @@ public class GamePanel extends JPanel implements Runnable {
 	}
 	
 	public void update() {
-		
-		
-		if(keyH.upPressed == true) {
-			playerY -= playerSpeed;
-		}
-		else if(keyH.downPressed == true) {
-			playerY += playerSpeed;
-		}
-		else if(keyH.leftPressed == true) {
-			playerX -= playerSpeed;
-		}
-		else if(keyH.rightPressed == true) {
-			playerX += playerSpeed;
-		}
+		player.update();
 	}
 	
 	public void paintComponent(Graphics g) {
 		super.paintComponent(g);
 		Graphics2D g2 = (Graphics2D)g;
 		
-		g2.setColor(Color.white);
-		g2.fillRect(playerX, playerY, tileSize, tileSize);
+		player.draw(g2);
 		
 		g2.dispose();
 	}
