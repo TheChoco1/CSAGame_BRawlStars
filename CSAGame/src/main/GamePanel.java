d2eaddb6634ffb7a73f84b85133c79fd57b607bc
richito-g <richardgeng1010@gmail.com>
2023-05-10 14:25:08 -0400

    GameMap + tiles

--- CSAGame/src/main/GamePanel.java
+++ CSAGame/src/main/GamePanel.java
@@ -8,6 +8,7 @@ import java.awt.Graphics2D;
 import javax.swing.JPanel;
 
 import entity.Player;
+import tile.TileManager;
 
 public class GamePanel extends JPanel implements Runnable {
 	
@@ -15,13 +16,14 @@ public class GamePanel extends JPanel implements Runnable {
 	final int scale = 3;
 	
 	public final int tileSize = originalTitleSize * scale;
-	final int maxScreenCol = 16;
-	final int maxScreenRow = 12;
-	final int screenWidth = tileSize * maxScreenCol;
-	final int screenHeight = tileSize * maxScreenRow;
+	public final int maxScreenCol = 16;
+	public final int maxScreenRow = 12;
+	public final int screenWidth = tileSize * maxScreenCol;
+	public final int screenHeight = tileSize * maxScreenRow;
 	
 	int FPS = 60;
 	
+	TileManager tileM = new TileManager(this);
 	KeyHandler keyH = new KeyHandler();
 	Thread gameThread;
 	Player player = new Player(this, keyH);
@@ -77,6 +79,8 @@ public class GamePanel extends JPanel implements Runnable {
 	public void paintComponent(Graphics g) {
 		super.paintComponent(g);
 		Graphics2D g2 = (Graphics2D)g;
+		
+		tileM.draw(g2);
 		
 		player.draw(g2);
 		
--- CSAGame/src/tile/TileManager.java
+++ CSAGame/src/tile/TileManager.java
@@ -1,7 +1,10 @@
 package tile;
 
 import java.awt.Graphics2D;
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 
 import javax.imageio.ImageIO;
 
@@ -12,10 +15,14 @@ public class TileManager {
 	GamePanel gp;
 	Tile[] tile;
 	
+	int mapTileNum[][];
 	public TileManager(GamePanel gp) {
 		this.gp = gp;
 		
 		tile = new Tile[10];
+		mapTileNum = new int[gp.maxScreenCol][gp.maxScreenRow];
+		getTileImage();
+		loadMap();
 	}
 	
 	public void getTileImage() {
@@ -35,7 +42,39 @@ public class TileManager {
 			e.printStackTrace();
 		}
 	}
-
+	public void loadMap() {
+		
+		try {
+			InputStream is = getClass().getResourceAsStream("/maps/text01.txt");
+			BufferedReader br = new BufferedReader(new InputStreamReader(is));
+			
+			int col = 0;
+			int row = 0;
+			
+			while(col<gp.maxScreenCol && row<gp.maxScreenRow) {
+				String line  = br.readLine();
+				
+				while(col <gp.maxScreenCol) {
+					String numbers[] = line.split(" ");
+					
+					int num = Integer.parseInt(numbers[col]);
+					
+					mapTileNum[col][row] = num;
+					col++;
+				}
+				if(col == gp.maxScreenCol) {
+					col = 0;
+					row++;
+				}
+			}
+			br.close();
+		}catch(Exception e) {
+			
+		}
+		
+		
+		
+	}
 	public void draw(Graphics2D g2) {
 		
 		int col = 0;
@@ -44,8 +83,18 @@ public class TileManager {
 		int y = 0;
 		
 		while(col<gp.maxScreenCol && row<gp.maxScreenRow) {
+			int tileNum = mapTileNum[col][row];
 			
+			g2.drawImage(tile[tileNum].image, x, y, gp.tileSize, gp.tileSize,null);
+			col++;
+			x += gp.tileSize;
 			
+			if(col == gp.maxScreenCol) {
+				col = 0;
+				x = 0;
+				row++;
+				y += gp.tileSize;
+			}
 		}
 		
 	}
